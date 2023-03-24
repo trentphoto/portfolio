a4ec5506da8ab08e0ad4958ee5a93ec0e00d8fbc
trentphoto <trentphoto@gmail.com>
2023-03-24 13:58:36 -0400

    initial commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link'
+import React from 'react'
+import Logo from '~/svg/logo.svg'
+
+export default function Nav() {
+  return (
+    <>
+        <div className="w-full relative shadow-md block py-4">
+            <div className="container flex items-center justify-between w-full">
+                <div className="items-center gap-1 hidden lg:flex">
+                    <Link href="/" className='flex gap-2 items-center font-fira text-2xl uppercase px-3 py-2 rounded-xl hover:bg-gray-100'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill='#434343' width={25} height={25} viewBox="0 0 576 512">
+                            {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                            <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
+                        </svg>
+                        Home
+                    </Link>
+                    <Link href="/about" className='flex gap-2 items-center font-fira text-2xl uppercase px-3 py-2 rounded-xl hover:bg-gray-100'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill='#434343' width={25} height={25} viewBox="0 0 512 512">
+                            {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/>
+                        </svg>
+                        About
+                    </Link>
+                    <Link href="/projects" className='flex gap-2 items-center font-fira text-2xl uppercase px-3 py-2 rounded-xl hover:bg-gray-100'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill='#434343' width={25} height={25} viewBox="0 0 512 512">
+                            {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                            <path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/>
+                        </svg>
+                        Projects
+                    </Link>
+                </div>
+
+                {/* logo */}
+                <Logo width={220} height={50} />
+
+                <div className="flex items-center gap-1">
+                    <div className="w-28"></div>
+                    <Link href="https://github.com/trentphoto" className='flex gap-2 items-center font-fira text-2xl uppercase px-3 py-2 rounded-xl hover:bg-gray-100'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill='#434343' width={25} height={25} viewBox="0 0 496 512">
+                            {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+                        </svg>
+                        Github
+                    </Link>
+                    <Link href="https://github.com/trentphoto" className='flex gap-2 items-center font-fira text-2xl uppercase px-3 py-2 rounded-xl hover:bg-gray-100'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill='#434343' width={25} height={25} viewBox="0 0 384 512">
+                            {/* <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                            <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/>
+                        </svg>
+                        Resume
+                    </Link>
+                </div>
+
+
+
+            </div>
+        </div>
+    </>
+
+
+
+  )
+}
+
+
+
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import Nav from '@/components/Nav';
+
+export default function HomePage() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
