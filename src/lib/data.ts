70d5411eae44e4e5f230ea337d13a2abf27f76a0
trentphoto <trentphoto@gmail.com>
2023-07-05 16:34:58 -0400

    Update Resume

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 export const data = {
     github: "https://github.com/trentphoto",
     email: "james@jamestrent.net",
-    resumePDF: "https://res.cloudinary.com/dakfmjumy/image/upload/v1680010696/jamestrent.net/James_Trent_-_Resume_k5j1p4.pdf",
+    resumePDF: "https://res.cloudinary.com/dakfmjumy/image/upload/v1688589230/jamestrent.net/James_Trent_-_Resume_-_frontend_vjpven.pdf",
 }
 
 export const nav = {
